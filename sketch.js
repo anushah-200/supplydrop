7cc4e57d78fd90c2d38b0b0538a2605170f65583
anushah-200 <77103799+anushah-200@users.noreply.github.com>
2021-01-21 16:08:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.2,isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:true});
 	World.add(world, packageBody);
 	
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
@@ -59,6 +59,5 @@ Matter.Body.setStatic(packageBody,false)
     
   }
 }
-
 
 
